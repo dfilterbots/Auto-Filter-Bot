688079f0bb387a153c52b8e27da81d65204a49db
Jazin <ddfilterbot01@gmail.com>
2025-08-07 18:38:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ PM_SEARCH = bool(environ.get('PM_SEARCH', True))
 EMOJI_MODE = bool(environ.get('EMOJI_MODE', False)) 
 PAID_STREAM = bool(environ.get('PAID_STREAM', False)) 
 STREAM_MODE = bool(environ.get('STREAM_MODE', False))
-MAINTENANCE_MODE = bool(environ.get('MAINTENANCE_MODE', Ture)) 
+MAINTENANCE_MODE = bool(environ.get('MAINTENANCE_MODE', False)) 
 
 
 IGNORE_WORDS = (list(os.environ.get("IGNORE_WORDS").split(",")) if os.environ.get("IGNORE_WORDS") else []) #Remove Words While Searching Files
