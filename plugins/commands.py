3cb3c7b77f9a6399eb792f8ae3a88d646841d708
Jazin <ddfilterbot01@gmail.com>
2025-08-07 18:34:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -289,11 +289,11 @@ async def start(client, message):
             if STREAM_MODE:
                 btn = [
                     [InlineKeyboardButton('𝖦𝖾𝗇𝖾𝗋𝖺𝗍𝖾 𝖲𝗍𝗋𝖾𝗆𝗂𝗇𝗀 𝖫𝗂𝗇𝗄', callback_data=f'streamfile:{file_id}')],
-                    [InlineKeyboardButton('🖥 𝙾ᴛᴛ 𝚄ᴩᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🖥', url=UPDATE_CHANNEL_LNK)]  
+                    [InlineKeyboardButton('🖥 𝙾ᴛᴛ 𝚄ᴩᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🖥', url=CHNL_LNK)]  
                 ]
             else:
                 btn = [
-                    [InlineKeyboardButton('🖥 𝙾ᴛᴛ 𝚄ᴩᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🖥', url=CHENL_LNK)]
+                    [InlineKeyboardButton('🖥 𝙾ᴛᴛ 𝚄ᴩᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🖥', url=CHNL_LNK)]
                 ]
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
