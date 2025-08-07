6f511cf62ff91579a6dd09e1163e374ab1319cd8
Jazin <ddfilterbot01@gmail.com>
2025-08-07 18:09:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -246,7 +246,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b>{file_name}\nUploaded By: <a herf="https://t.me/SilentXBotz">[SilentXBotz]</a></b>"""
+    CAPTION = """<b>{caption}</b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
